78ce4d2caeba041a9f3375b24b6c08d2cfeb9e36
Craig Humphreys <craig_h_411@yahoo.co.uk>
2017-02-24 16:27:42 +0000

    Added Hot-Reloading

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 import sass from "gulp-sass";
+import hmr from 'browserify-hmr';
+import watchify from 'watchify';
 
 gulp.task('default', () => {
     return browserify({
@@ -13,8 +15,28 @@ gulp.task('default', () => {
         .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./resources/assets/sass/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+gulp.task('hmr', () => {
+    const b = browserify({
+        entries: 'resources/assets/js/app.js',
+        plugin: [hmr, watchify],
+        debug: true
+    })
+
+    b.on('update', bundle);
+    bundle();
+
+    function bundle() {
+        b.bundle()
+            .on('error', err => {
+                util.log("Browserify Error", util.colors.red(err.message))
+            })
+            .pipe(source('app.js'))
+            .pipe(gulp.dest('public/js'));
+    }
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./resources/assets/sass/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
 });
--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -2,7 +2,7 @@
     <div class="ui center aligned padded grid">
         <div class="column">
             <div class="ui segment">
-                <h1>Semantic-UI Setup Complete</h1>
+                <h1>Semantic-UI Setup Complete!</h1>
             </div>
         </div>
     </div>
